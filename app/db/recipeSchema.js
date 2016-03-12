1acede4b9f869da37906e456c9eb491c244de3d7
Bruendermiester <jonbruenderman@hotmail.com>
2016-03-12 15:37:33 -0800

    Adding schema for recipe

---
+++ app/db/recipeSchema.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+  image: {type: String},
+  title: {type: String, required: true, maxlength: 140},
+  ingredients: {type: Array, required: true,},
+  instructions: {type: String, required: true, maxlength: 140},
+  time: {type: String, required: true},
+  favorites: {type: Array},
+  meal: {type: String, required: true},
+  mainIngredient : {type: String, required: true},
+  notes: {type: String},
+
+});
+
+module.exports = schema;
