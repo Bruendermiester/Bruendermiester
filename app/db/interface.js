1162e1f409ff6704186bdb2c6d83376fcc87f999
Bruendermiester <jonbruenderman@hotmail.com>
2016-03-14 20:15:27 -0700

    separation

--- app/db/interface.js
+++ app/db/interface.js
@@ -4,22 +4,30 @@ exports.insert = function(db, doc, callback) {
 			console.log(error, "insert fail");
 			process.exit(1);
 		}
-		var query = { title : "Chicken Alfrado"};
-		db.collection('recipe').find(query).toArray( function(err, docs) {
-			if(err) {
-				console.log("bummer");
-				process.exit(1);
-			}
-			console.log("found docs:");
-			docs.forEach(function(doc) {
-				console.log(JSON.stringify(doc));
-			});
-			process.exit(0);
-
-		});
 
 	})
-	// db.collection('recipe').remove({}, function(error){
+};
 
-	// })
+exports.sortByPerson = function(db, name, callback) {
+	var query = {favorites : name};
+	db.collection('recipe').find().toArray( function(err, docs) {
+
+		console.log("here");
+		if(err) {
+			console.log("bummer");
+			process.exit(1);
+		}
+		console.log("found docs:");
+		docs.forEach(function(doc) {
+			console.log(JSON.stringify(doc));
+		});
+
+	});
+};
+
+exports.remove = function(db, callback) {
+
+	db.collection('recipe').remove({}, function(error){
+
+	})
 };
--- app/index.js
+++ app/index.js
@@ -6,24 +6,33 @@ var recipes = {
       title: "Chicken Alfrado",
       ingredients: [],
       time: "1 hour",
-      favorites: ["Andrea", "Adam", "Ashley"],
+      favorites: ["Andrea"],
       meal: "Dinner",
       mainIngredient: "Chicken",
       notes: "It is very good"	
-}
+};
 var mongodb = require('mongodb');
 
 var uri = 'mongodb://localhost:27017/testing';
 
  mongodb.MongoClient.connect(uri, function(err, db) {
 
-
-
-	dbInterface.insert(db, recipes, function(error){
-		if(error){
+	dbInterface.insert(db, recipes, function(error) {
+		if(error) {
 			console.log("error-->", error);
 		}
-	})
+	});
+	dbInterface.sortByPerson(db, "Adam", function(error) {
+		if(error) {
+			console.log("error-->", error);
+		}
+		//process.exit(0);
+	});
+	// dbInterface.remove(db, function(error){
+	// 	if(error){
+	// 		console.log("error-->", error);
+	// 	}
+	// });
  });
 
 
