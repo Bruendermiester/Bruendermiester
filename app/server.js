1f01530bf494efedfce59a048c224348846deb73
Bruendermiester <jonbruenderman@hotmail.com>
2016-04-08 17:42:57 -0700

    try again

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = __dirname;
 var mongoose = require('mongoose'); 
-var uri = 'mongodb://<heroku_c13sjx0h>:<brokensword=99>@ds019980.mlab.com:19980/heroku_c13sjx0h';
+var uri = 'mongodb://heroku_c13sjx0h:brokensword=99@ds019980.mlab.com:19980/heroku_c13sjx0h';
 var bodyParser = require('body-parser');
 
 mongoose.connect(uri);  
@@ -31,7 +31,7 @@ app.get('/', function(req, res) {
 
 app.get('/api/recipes', function(req, res) {
 
-	console.log("here")
+	console.log("here");
 
 	var filterObject = {};
 
